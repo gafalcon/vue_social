0a57294b888af9c4f68905b1ac64ae219388c576
Gabriel Falcones <gafalcon@espol.edu.ec>
2021-08-08 19:55:54 -0500

    Profile Sidebar

---
+++ src/components/account_sidebar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="py-4 px-2 flex justify-between items-center">
+    <ProfileImg size="14" :imgSrc="user.profilePicture" />
+    <div>
+      <p class="font-bold">{{ user.username }}</p>
+      <p class="text-sm text-gray-500">{{ user.name }}</p>
+    </div>
+    <a href="" class="text-indigo-900 text-sm font-bold">Switch</a>
+  </div>
+</template>
+
+<script>
+import ProfileImg from "./profileImg.vue";
+export default {
+  name: "AccountSidebar",
+  components: { ProfileImg },
+  props: {
+    user: {
+      type: Object,
+      default() {
+        return {
+          username: "gafalcon",
+          profilePicture: "assets/person/1.jpeg",
+          name: "Gabriel Falcones Paredes",
+        };
+      },
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="container mt-5 mx-auto px-36 grid grid-cols-3 gap-4">
+  <div class="container mt-7 mx-auto px-36 grid grid-cols-3 gap-4">
     <div class="feed col-span-2">
       <Moments />
     </div>
 
-    <div class="sidebar">Sidebar</div>
+    <div class="sidebar">
+      <AccountSidebar />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Moments from "@/components/moments";
+import AccountSidebar from "@/components/account_sidebar";
 export default {
   name: "Home",
-  components: {Moments},
+  components: { Moments, AccountSidebar },
 };
 </script>
